9f3c1e1392ea746c1bbd63dda31f09512bb0709f
Janha-vi <143474269+Janha-vi@users.noreply.github.com>
2024-10-03 23:49:08 +0530

    Time: 20 ms (11.36%), Space: 11.7 MB (76.44%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -13,12 +13,17 @@ class Solution(object):
         #     else:
         #         last = len(s)    
         #     break
+        
         # Replace the above 6 (!!!!) lines with this one line:
         while s[last] == " ": last-=1
 
+        # Gregg solution
         # for i in range(last+1):
         #     if s[i] != " ":
-        #         count+=1        
+        #         count+=1
+        #     else:
+        #         count = 0
+        # return count         
 
         while r < last:
             if s[r] == " ":
